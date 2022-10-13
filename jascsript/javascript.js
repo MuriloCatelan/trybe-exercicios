efe9f8cb662020bf6de7d367a24c6a34fa92ad10
Murilo Vinicius Carrara Catelan <murilocatelan@gmail.com>
2022-10-13 20:17:09 -0300

    Exercicios de Algoritmos

--- jascsript/javascript.js
+++ jascsript/javascript.js
@@ -45,17 +45,54 @@
 // console.log(resultado)
 
 
-var numero = 50;
-var resposta = numero;
-var divisores=0;
+// Exercicios de numeros primos
 
-  for(var count=1 ; count<=numero ; count++)
-   if(numero % count == 0)
-   	divisores++;
+// let inicio = 2;
+// let fim = 50;
+// const arrayDosCria = [];
+// let divisores;
+// let resultado;
+// for(let count = 2; count <= fim; count += 1) {
+//   divisores=0;
+//   for(let aux = 2; aux <= count; aux += 1){
+//     if(count % aux === 0){
+//       divisores += 1;
+//     }
+//   }
+//   if(divisores <= 1){
+//     arrayDosCria.push(count)
+//   }
+// }
+// console.log(arrayDosCria);
+
+
+//Inversão de string
+
+// let word = 'banana';
+// let stringAux = '';
+//     for(let index = word.length - 1; index >= 0; index -= 1) {
+//       stringAux = stringAux.concat(word[index]);
+//     }
+// console.log(stringAux);
+
+
+//Utilize a estrutura de repetição for para escrever dois algoritmos: um que retorne a maior palavra desse array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maior = '';
+// let menor = array[0];
+//   for(let index = 0; index <= array.length - 1; index += 1) {
+//     if(array[index].length > maior.length){
+//       maior = array[index];
+//     }
+//     if(array[index].length < menor.length){
+//       menor = array[index];
+//     }
+    
+//   }
+// console.log(maior);
+// console.log(menor);
+
+
+
   
-  if(divisores==2){
-  	resposta ='É primo';
-}
-  else{
-  	resposta ='Não é primo';
-}
