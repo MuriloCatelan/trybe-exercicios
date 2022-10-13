915a6327990f7b339a309439ae203b1d98d73119
Murilo Vinicius Carrara Catelan <murilocatelan@gmail.com>
2022-10-13 15:12:49 -0300

    Exercicios iniciais completos

--- jascsript/javascript.js
+++ jascsript/javascript.js
@@ -17,10 +17,10 @@
 
 //Exercicio 2
 
-const n = 5;
+// const n = 5;
 
-let resultado = 0;
-for(let index = 0; index <= n; index += 1) {
-    resultado =+ index
-};
-console.log(resultado)
+// let resultado = 0;
+// for(let index = 0; index <= n; index += 1) {
+//     resultado =+ index
+// };
+// console.log(resultado)
