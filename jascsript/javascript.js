881ad8a603fc2d8aefbb65465412c443db30a789
Murilo Vinicius Carrara Catelan <murilocatelan@gmail.com>
2022-10-13 15:11:59 -0300

    Dia 3 de javascript, lógica de programação e algoritmos - primeiros exercicios

---
+++ jascsript/javascript.js
@@ -0,0 +1,26 @@
+//Exercicio 1 do dia 13-10-2022
+
+// let fruits = [3, 4, 1, 1, 1];
+
+// let sum = 0
+// for(let p = 0; p < fruits.length; p +=1 ) {
+//     sum += fruits[p];
+// }
+
+// if(sum > 15) {
+//     console.log(sum)
+// }
+// else {
+//     console.log("Valor menor que 16")
+// }
+
+
+//Exercicio 2
+
+const n = 5;
+
+let resultado = 0;
+for(let index = 0; index <= n; index += 1) {
+    resultado =+ index
+};
+console.log(resultado)
